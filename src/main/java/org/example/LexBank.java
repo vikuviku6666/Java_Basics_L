38e0c3cf78e5098a78328ccf17ea8b86ba6eeb87
Vinay <53383503+vikuviku6666@users.noreply.github.com>
2024-12-27 20:36:22 +0100

    added singleton design pattern to Bank class

--- src/main/java/org/example/LexBank.java
+++ src/main/java/org/example/LexBank.java
@@ -1,25 +1,37 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
+// Singleton Design Pattern
 public class LexBank
 {
+    private static LexBank instance;
     private static ArrayList<LexAccount> accounts = new ArrayList<>();
 
     private LexBank()
     {
     }
 
-    public static ArrayList<LexAccount> getAccounts()
+    public synchronized static LexBank getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new LexBank();
+        }
+        return instance;
+    }
+
+    public ArrayList<LexAccount> getAccounts()
     {
         return accounts;
     }
 
-    public static void setAccounts(ArrayList<LexAccount> accounts)
+    public void setAccounts(ArrayList<LexAccount> accounts)
     {
         LexBank.accounts = accounts;
     }
-    public static void AccountInfo()
+    public void AccountInfo()
     {
         for (LexAccount account : accounts)
         {
@@ -29,5 +41,10 @@ public class LexBank
             System.out.println("===================================");
 
         }
+    }
+
+    public Object clone() throws CloneNotSupportedException
+    {
+        throw new CloneNotSupportedException();
     }
 }
--- src/main/java/org/example/Main1.java
+++ src/main/java/org/example/Main1.java
@@ -2,26 +2,33 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main1
 {
-    public static void main(String[] args)
-    {
+    public static ArrayList<LexAccount> accounts(){
         LexAccount account1 = new LexAccount("John Doe", 1000);
         LexAccount account2 = new LexAccount("Jane Doe", 2000);
         LexAccount account3 = new LexAccount("Karl Doe", 3000);
         LexAccount account4 = new LexAccount("Lars Doe", 4000);
         LexAccount account5 = new LexAccount("Gustav Doe", 5000);
         LexAccount account6 = new LexAccount("David Doe", 6000);
+        return new ArrayList<>(Arrays.asList(account1, account2, account3, account4, account5, account6));
+    }
 
-        LexBank.setAccounts(new ArrayList<>(Arrays.asList(account1, account2, account3, account4, account5, account6)));
-        ArrayList<LexAccount> accounts = LexBank.getAccounts();
+    public static void main(String[] args) throws CloneNotSupportedException
+    {
+        LexBank lexBank = LexBank.getInstance();
+        //lexBank.clone();
+
+        lexBank.setAccounts(accounts());
+        ArrayList<LexAccount> accounts = lexBank.getAccounts();
         for (LexAccount account : accounts)
         {
             account.deposit(Math.random() * 1000);
             account.withdraw(Math.random() * 500);
         }
         System.out.println("===================================");
-        LexBank.AccountInfo();
+        lexBank.AccountInfo();
     }
 }
